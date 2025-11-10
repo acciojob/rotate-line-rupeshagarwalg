4afd406dc3f1d2ad8ea0e784f0c9edb3c39cc2d9
rupeshagarwalg <142991136+rupeshagarwalg@users.noreply.github.com>
2025-11-10 15:55:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const line = document.getElementById('line');
+let angle = 0;
+
+setInterval(() => {
+  angle += 2; // increase angle by 2 degrees
+  line.style.transform = translate(-50%, -50%) rotate(${angle}deg);
+}, 20); // update every 20 milliseconds
